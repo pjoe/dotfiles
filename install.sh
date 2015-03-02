ef1be9c05af6b9b437d751482d573f48c7ebe5f7
Pelle Johnsen <pelle.johnsen@gmail.com>
2015-03-02 11:10:28 +0100

    Also add .Xdefaults to install script

--- install.sh
+++ install.sh
@@ -10,4 +10,5 @@ export DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ln -sfv "$DOTFILES_DIR/hg/.hgrc" ~
 ln -sfv "$DOTFILES_DIR/tmux/.tmux.conf" ~
 ln -sfv "$DOTFILES_DIR/vim/.vimrc" ~
+ln -sfv "$DOTFILES_DIR/X/.Xdefaults" ~
 
